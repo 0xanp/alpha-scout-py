d9f134c8851d60d4ebfe8415ab9c175a905ab768
Binh An <anbinhpham110799@gmail.com>
2023-01-30 10:41:27 +0700

    exception handling

--- google_sheets_reader.py
+++ google_sheets_reader.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class GoogleSheetReader:
     def __init__(self):
-        if not os.environ.get('GOOGLE_SHEET_MIDNIGHT_LABS_SHEET_ID') and not os.environ.get('GOOGLE_SHEET_MIDNIGHT_LABS_GID'):
+        if not os.environ.get('GOOGLE_SHEET_URL'):
             raise ValueError("No env variable for google sheets")
         self.spreadsheet_url= os.environ.get('GOOGLE_SHEET_URL')
     async def read_data(self):
